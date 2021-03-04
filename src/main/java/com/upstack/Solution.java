9ee344255f04fc8ec885e43d8b078aedb949cd7e
Mohamed ElBahrawy <eng.mohamed.morsy.mohamed@gmail.com>
2021-03-04 13:53:26 +0200

    final solution

--- src/main/java/com/upstack/Solution.java
+++ src/main/java/com/upstack/Solution.java
@@ -1,18 +1,43 @@
 package com.upstack;
 
 public class Solution {
-    
-    public static String formatString(int[] input) {
-        String result = "";
-        for (int i=0; i < input.length; i++) {
-            int count = 1;
-            while (i + 1 < input.length && input[i] == input[i + 1]) {
-                i++;
-                count++;
-            }
-            result +=  count > 1 ? " " + input[i] + " FOR " + count: " " + input[i];
+
+    public String formatString(int[] input) {
+        if (input.length == 0) {
+            return "";
         }
+        String result =  visit(input, 0, input.length - 1);
         return result.trim();
+    }
+
+    private String visit(int[] input, int start, int end) {
+        int pivot = (start + end) / 2;
+        int count = 1;
+        String leftResult = "";
+        String rightResult = "";
+        int i = pivot + 1;
+        int j = pivot - 1;
+        while (true) {
+            if ((i > end || input[pivot] != input[i]) && (j < start || input[pivot] != input[j])) {
+                break;
+            }
+            if (i <= end && input[pivot] == input[i]) {
+                count++;
+                i++;
+            }
+            if (j >= start && input[pivot] == input[j]) {
+                count++;
+                j--;
+            }
+        }
+        if (j >= start) {
+            leftResult = visit(input, start, j);
+        }
+        if (i <= end) {
+            rightResult = visit(input, i, end);
+        }
+        String result = leftResult + (count > 1 ? " " + input[pivot] + " FOR " + count : " " + input[pivot]) + rightResult;
+        return result;
     }
 
 }
--- src/test/java/com/upstack/SolutionTest.java
+++ src/test/java/com/upstack/SolutionTest.java
@@ -2,27 +2,95 @@ package com.upstack;
 
 import org.junit.jupiter.api.Test;
 
-import static com.upstack.Solution.formatString;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SolutionTest {
 
    @Test
    public void testcase1() {
-       String result = formatString(new int[]{2, 2, 0, 0, 0, 0, 0, 1});
+       String result = new Solution().formatString(new int[]{2, 2, 0, 0, 0, 0, 0, 1});
        assertEquals("2 FOR 2 0 FOR 5 1", result);
    }
 
     @Test
     public void testcase2() {
-        String result = formatString(new int[]{1,1,1,1,1,0});
+        String result = new Solution().formatString(new int[]{1,1,1,1,1,0});
         assertEquals("1 FOR 5 0", result);
     }
 
     @Test
     public void testcase3() {
-        String result = formatString(new int[]{1,2,3,4,6,5,5,5,5,5,5,5,5,5,5});
+        String result = new Solution().formatString(new int[]{1,2,3,4,6,5,5,5,5,5,5,5,5,5,5});
         assertEquals("1 2 3 4 6 5 FOR 10", result);
+    }
+
+    @Test
+    public void testcase4() {
+        String result = new Solution().formatString(new int[]{5,5,5,5,5,5,5,5,5,5});
+        assertEquals("5 FOR 10", result);
+    }
+
+    @Test
+    public void testcase5() {
+        String result = new Solution().formatString(new int[]{5,5});
+        assertEquals("5 FOR 2", result);
+    }
+
+    @Test
+    public void testcase6() {
+        String result = new Solution().formatString(new int[]{5});
+        assertEquals("5", result);
+    }
+
+    @Test
+    public void testcase7() {
+        String result = new Solution().formatString(new int[]{5,4,2});
+        assertEquals("5 4 2", result);
+    }
+
+    @Test
+    public void testcase8() {
+        String result = new Solution().formatString(new int[]{5,4});
+        assertEquals("5 4", result);
+    }
+
+    @Test
+    public void testcase9() {
+        String result = new Solution().formatString(new int[]{});
+        assertEquals("", result);
+    }
+
+    @Test
+    public void testcase10() {
+        String result = new Solution().formatString(new int[]{1,2,3,4,6,5,5,5,5,5,5,5,5,5,5,1,2,3,4,6});
+        assertEquals("1 2 3 4 6 5 FOR 10 1 2 3 4 6", result);
+    }
+
+    @Test
+    public void testcase11() {
+        String expected = "";
+        int[] input = new int[100000];
+        for (int i=0; i < input.length; i++) {
+            input[i] = (int) Math.round(Math.random() * 100);
+        }
+        long t1 = System.currentTimeMillis();
+        for (int i=0; i < input.length; i++) {
+            int count = 1;
+            while (i + 1 < input.length && input[i] == input[i + 1]) {
+                i++;
+                count++;
+            }
+            expected +=  count > 1 ? " " + input[i] + " FOR " + count: " " + input[i];
+        }
+        long t2 = System.currentTimeMillis();
+        String  result = new Solution().formatString(input);
+        long t3 = System.currentTimeMillis();
+        assertEquals(result, expected.trim());
+        long td1 = t2-t1;
+        long td2 = t3-t2;
+        System.out.println(td1);
+        System.out.println(td2);
+        assertTrue(t2-t1 > t3-t2);
     }
 
 }
